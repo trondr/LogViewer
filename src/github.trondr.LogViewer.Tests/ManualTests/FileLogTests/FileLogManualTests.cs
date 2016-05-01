3ba718f5608e4ffef794fb5f2e13def4cdb89fca
trondr <trondr@outlook.com>
2016-05-01 14:03:55 +0200

    File flush bug demonstration and workaround.

--- src/github.trondr.LogViewer.Tests/ManualTests/FileLogTests/FileLogManualTests.cs
+++ src/github.trondr.LogViewer.Tests/ManualTests/FileLogTests/FileLogManualTests.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 using System.Threading;
 using Common.Logging;
 using Common.Logging.Simple;
 using github.trondr.LogViewer.Infrastructure;
 using github.trondr.LogViewer.Library.Module.Commands.OpenLog;
+using Microsoft.Win32.SafeHandles;
 using NUnit.Framework;
 
 namespace github.trondr.LogViewer.Tests.ManualTests.FileLogTests
@@ -39,6 +43,50 @@ namespace github.trondr.LogViewer.Tests.ManualTests.FileLogTests
             }
         }
 
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [ResourceExposure(ResourceScope.None)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool FlushFileBuffers(SafeFileHandle hFile);
+
+
+        [Test]
+        public void FileFlushTest_FlushBugWorkaround()
+        {
+            var testMessage = new string('T', 1024);
+            var logFile = Environment.ExpandEnvironmentVariables(@"%public%\Logs\FileLogTests-FlushedWorkaround-%COMPUTERNAME%-%USERNAME%-CustomLog.log");
+            using (var sw = new StreamWriter(logFile))
+            {
+                while (true)
+                {
+                    sw.AutoFlush = true;
+                    var fs = sw.BaseStream as FileStream;
+                    if(fs != null)
+                    {
+                        FlushFileBuffers(fs.SafeFileHandle); //Now the file is flushed to disk! Watch in Windows Explorer and see that file size is changing on the file without pressing F5.
+                    }
+                    sw.WriteLine(testMessage);
+                    Thread.Sleep(500);
+                }                
+            }
+        }
+
+        [Test]
+        public void FileFlushTest_FlushBug()
+        {
+            var testMessage = new string('T', 1024);
+            var logFile = Environment.ExpandEnvironmentVariables(@"%public%\Logs\FileLogTests-NotFlushedBug-%COMPUTERNAME%-%USERNAME%-CustomLog.log");
+            using (var sw = new StreamWriter(logFile))
+            {
+                while (true)
+                {
+                    sw.AutoFlush = true; // Bug: The file is not actually flushed to disk.                    
+                    sw.WriteLine(testMessage);
+                    sw.Flush(); // Bug: This does not work either. Watch in Windows Explorer and see that file size is NOT changing. F5 is required to trigger flush to disk.
+                    Thread.Sleep(500);
+                }                
+            }
+        }
+        
         private static void StoppTcpTestLogger(TestFileLogGenerator logGenerator)
         {
             logGenerator.Stop();
