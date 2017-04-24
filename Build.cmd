80f97c33190cbe4f884b4ee30e4536234e167196
trondr <trondr@outlook.com>
2017-04-24 15:49:20 +0200

    Updated build script to use VS2017

--- Build.cmd
+++ Build.cmd
@@ -1,30 +1,60 @@
 @ECHO OFF
 Set ProductName=LogViewer
 Set SolutionName=LogViewer
+Set Set BuildMessage=
 
-IF EXIST "%VSDEVCMD%" goto Build
-IF EXIST "%MSBUILDPATH%" goto Build
+IF EXIST "%VSDEVCMD%" Goto Build
+IF EXIST "%MSBUILDPATH%" Goto Build
 
-:VSEnv
-Set VSDEVCMD=%VS140COMNTOOLS%VsDevCmd.bat
-Echo Checking to see if Visual Studio 2015 is installed ("%VS140COMNTOOLS%")
-IF NOT EXIST "%VSDEVCMD%" set BuildMessage="Visual Studio 2015 do not seem to be installed, trying MSBuild instead..." & goto MSBuildEnv
-Echo Preparing build environment...
+:VS2017Env
+Echo %BuildMessage%
+REM Due to bug(?) in VS 2017 not setting the VS150COMNTOOLS environment  
+REM variable, try finding the tools directory path using brute force
+Set ToolsDir=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\
+IF EXIST "%ToolsDir%" Set VS150COMNTOOLS=%ToolsDir%
+IF EXIST "%ToolsDir%" Goto VS2017EnvPrep
+Set ToolsDir=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\
+IF EXIST "%ToolsDir%" Set VS150COMNTOOLS=%ToolsDir%
+IF EXIST "%ToolsDir%" Goto VS2017EnvPrep
+Set ToolsDir=C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\
+IF EXIST "%ToolsDir%" Set VS150COMNTOOLS=%ToolsDir%
+IF EXIST "%ToolsDir%" Goto VS2017EnvPrep
+Set ToolsDir=C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\
+IF EXIST "%ToolsDir%" Set VS150COMNTOOLS=%ToolsDir%
+IF EXIST "%ToolsDir%" Goto VS2017EnvPrep
+Set ToolsDir=C:\Program Files\Microsoft Visual Studio\2017\Community\Common7\Tools\
+IF EXIST "%ToolsDir%" Set VS150COMNTOOLS=%ToolsDir%
+IF EXIST "%ToolsDir%" Goto VS2017EnvPrep
+Set ToolsDir=C:\Program Files\Microsoft Visual Studio\2017\Professional\Common7\Tools\
+IF EXIST "%ToolsDir%" Set VS150COMNTOOLS=%ToolsDir%
+IF EXIST "%ToolsDir%" Goto VS2017EnvPrep
+Set ToolsDir=C:\Program Files\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\
+IF EXIST "%ToolsDir%" Set VS150COMNTOOLS=%ToolsDir%
+IF EXIST "%ToolsDir%" Goto VS2017EnvPrep
+Set ToolsDir=C:\Program Files\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\
+IF EXIST "%ToolsDir%" Set VS150COMNTOOLS=%ToolsDir%
+IF EXIST "%ToolsDir%" Goto VS2017EnvPrep
+
+:VS2017EnvPrep
+Set VSDEVCMD=%VS150COMNTOOLS%VsDevCmd.bat
+Echo Checking to see if Visual Studio 2017 is installed (VSDEVCMD="%VSDEVCMD%")
+IF NOT EXIST "%VSDEVCMD%" Set BuildMessage=Visual Studio 2017 do not seem to be installed. Terminating.
+IF NOT EXIST "%VSDEVCMD%" Goto End
+Echo Visual Studio 2017 is installed, preparing build environment...
 call "%VSDEVCMD%"
-
-:MSBuildEnv
-Set MSBUILDPATH=%ProgramFiles(x86)%\MSBuild\14.0\Bin
-Echo Checking to see if MSBuild is installed ("%MSBUILDPATH%")
-IF NOT EXIST "%MSBUILDPATH%" set BuildMessage="Neither Visual Studio 2015 or MSBuild  seem to be installed. Terminating." & goto end
-Set Path=%Path%;%MSBUILDPATH%
+Goto Build
 
 :Build
-Echo Building %ProductName%...
+Echo Preparing to build %ProductName%...
+Echo Restoring nuget packages...
 nuget.exe restore %SolutionName%.sln
+Echo Finished restoring nuget packages.
+Echo Building %ProductName% using MSBuild...
 msbuild.exe %SolutionName%.build %1 %2 %3 %4 %5 %6 %7 %8 %9
 Set BuildErrorLevel=%ERRORLEVEL%
 IF %BuildErrorLevel%==0 Set BuildMessage=Sucessfully build %ProductName%
 IF NOT %BuildErrorLevel% == 0 Set BuildMessage=Failed to build %ProductName%
+Goto End
 
 :End
 Echo %BuildMessage%
